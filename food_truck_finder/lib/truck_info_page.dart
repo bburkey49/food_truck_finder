4090c560de48cf34c9041199447d037600bccd7a
Brian Burke <burke724@umn.edu>
2020-12-06 20:16:43 -0600

    changed visited to tried

--- food_truck_finder/lib/truck_info_page.dart
+++ food_truck_finder/lib/truck_info_page.dart
@@ -137,7 +137,7 @@ class _TruckInfoState extends State<TruckInfo> {
         onChanged:  (String newValueSelected ) {
           setState(() {
             personalizedTruck = newValueSelected;
-            if(newValueSelected == "Visited"){
+            if(newValueSelected == "Tried"){
               truckCol = Colors.lime[600];
               if(!visitedTrucks.any((element) => element.name == truck.name)){
                 visitedTrucks.add(truck);
